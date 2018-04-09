7df6f84002102664306ae71c2900b1c16bad5aeb
Vyacheslavick <37550293+Vyacheslavick@users.noreply.github.com>
2018-04-09 18:26:35 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,25 +1,14 @@
 package com.company;
 
-public class Book {
-    String name;
+public class Book extends Literature{
     String author;
-    int yearWereWritten;
 
-    public Book(String name, String author, int yearWereWritten) {
-        this.name = name;
+    public Book(String name, int year, String author) {
+        super(name, year);
         this.author = author;
-        this.yearWereWritten = yearWereWritten;
     }
-
-    public void printBook() {
-        System.out.println("Название книги: " + name);
+    public void print() {
+        super.print();
         System.out.println("Автор книги :" + author);
-        System.out.println("Год издания :" + yearWereWritten);
-        System.out.println("");
-    }
-    public boolean equalsYear(int yearWereWritten){
-        return this.yearWereWritten == yearWereWritten;
     }
 }
-
-
