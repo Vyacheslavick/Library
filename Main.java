a1e2b0ff1c72590515a54e84d3564aa1fe53141f
Vyacheslavick <37550293+Vyacheslavick@users.noreply.github.com>
2018-04-09 18:29:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,18 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Library librarybook = new Library(Generator.generateBook());
-        Library libraryMag = new Library(Generator.generateMagazine());
-        Library libraryYear = new Library(Generator.generateYearbook());
-        librarybook.printAllBooks();
-        libraryMag.printAllMag();
-        libraryYear.printAllYear();
+        Library libraryAll = new Library(Generator.generateData());
+        libraryAll.printAll();
+        System.out.println("");
         System.out.println("Введите желаемый год:");
         int search = Integer.parseInt(scan.nextLine());
-        librarybook.printEqualYearBook(search);
-        libraryMag.printEqualYearMag(search);
-        libraryYear.printEqualYearYB(search);
-        
-
+        libraryAll.printEqualYear(search);
     }
 }
