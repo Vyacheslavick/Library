945be45487a5c16787074c809fade92f62596e0f
Vyacheslavick <37550293+Vyacheslavick@users.noreply.github.com>
2018-04-09 18:28:58 +0300

    Update Magazine.java

--- Magazine.java
+++ Magazine.java
@@ -1,26 +1,17 @@
 package com.company;
 
-public class Magazine {
-    String name;
+public class Magazine extends Literature {
     String topic;
-    int year;
     int month;
-    public Magazine(String name, String topic, int year, int month){
-        this.name = name;
+
+    public Magazine(String name, int year, String topic, int month) {
+        super(name, year);
         this.topic = topic;
-        this.year = year;
         this.month = month;
-
     }
-    public void printMag() {
-        System.out.println("Название журнала: " + name);
+    public void print() {
+        super.print();
+        System.out.println( "Месяц:" + month);
         System.out.println("Тематика: " + topic);
-        System.out.println("Год: " + year + " Месяц:" + month);
-        System.out.println("");
     }
-    public boolean equalsYear(int year){
-        return this.year == year;
-    }
-
 }
-
