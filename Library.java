c451c54a32d8b6f4c9fe707daeb73dff522d6d71
Vyacheslavick <37550293+Vyacheslavick@users.noreply.github.com>
2018-04-09 18:28:16 +0300

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,64 +1,29 @@
 package com.company;
 
 public class Library {
-    Book[] books;
-    Magazine[] magazines;
-    Yearbook[] yearbooks;
+    Literature[][] literatures;
 
-    public Library(Book[] books) {
-        this.books = books;
+    public Library(Literature[][] literature) {
+        this.literatures = literature;
     }
 
-    public Library(Magazine[] magazines) {
-        this.magazines = magazines;
-    }
-
-    public Library(Yearbook[] yearbooks) {
-        this.yearbooks = yearbooks;
-    }
-
-    public void printAllBooks() {
-        for (Book book : books) {
-            book.printBook();
-        }
-    }
-
-    public void printAllMag() {
-        for (Magazine magazine : magazines) {
-            magazine.printMag();
-        }
-    }
-
-    public void printAllYear() {
-        for (Yearbook yearbook : yearbooks) {
-            yearbook.printYear();
-        }
-    }
-
-    public void printEqualYearBook(int year) {
-        for (Book book : books) {
-            if (book.equalsYear(year)) {
-                book.printBook();
+    public void printAll() {
+        for (Literature literature[] : literatures) {
+            for (int i = 0; i < 3; i++) {
+                literature[i].print();
             }
         }
     }
-
-    public void printEqualYearMag(int year) {
-        for (Magazine magazine : magazines) {
-            if (magazine.equalsYear(year)) {
-                magazine.printMag();
+    public void printEqualYear(int year) {
+        search:
+        for (Literature literature[] : literatures) {
+            for (int i = 0; i < 3; i++) {
+                if (literature[i].equalsYear(year)) {
+                    literature[i].print();
+                    break search;
+                }
             }
-        }
-    }
 
-    public void printEqualYearYB(int year) {
-        for (Yearbook yearbook : yearbooks) {
-            if (yearbook.equalsYear(year)) {
-                yearbook.printYear();
-            }
         }
     }
 }
-
-
-
